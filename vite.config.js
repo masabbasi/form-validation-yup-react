7dcacb893a7265e73f22a3ee1accfa09ce6323c3
Your Namemasabbasi <masoud.abbasi.71@gmail.com>
2023-06-09 00:34:52 +0330

    All

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/form-validation-formik-react/",
+	base: "/form-validation-yup-react/",
   plugins: [react()],
 })
